47d69da247d25481caec4023e668a28ea1bba948
SolarPolarMan <solarpolarman@gmail.com>
2016-04-21 21:03:09 +0100

    It WORKS!

--- jssource.js
+++ jssource.js
@@ -1,21 +1,22 @@
-
-$(document).ready(function(){
-
-});
-
-$(document).on('input',$('#givenWord'),function () {
-  if (typeof ans == 'undefined'){
-    var ans = new Array;
-      $.get('preceed_with_an.txt', function(data){
-              var array = data.split('\n');
-              console.log(ans);
-          });
-  }
-  else{
-    for (var i = 0; i < ans.length; i++){
-      if (ans[i] == $('#givenWord').lower){
-        console.log(ans[i])
-      }
+var word = ""
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+      word = $('#givenWord').val();
+      word = word.toLowerCase()
+      console.log(word)
+      var ans = new Array;
+      $.get('/preceed_with_an.txt', function(data){
+        var ans = data.split('\n');
+        var answer = $("#answer");
+        for (var i = 0; i < ans.length; i++){
+          if (ans[i] === word){
+            $(answer).text("An");
+            console.log("An")
+            break;
+          }else{
+            $(answer).text("A");
+          }
+        }
+      });
     }
-  }
 });
