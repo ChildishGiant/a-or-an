e727ddbe278d46a1d4c3da4276d99ca532bb8264
SolarPolarMan <solarpolarman@gmail.com>
2016-04-22 10:22:39 +0100

    now works with spaces and two words.

--- jssource.js
+++ jssource.js
@@ -1,9 +1,15 @@
 var word = ""
+var arr = []
 $(document).keypress(function(e) {
     if(e.which == 13) {
+      
       word = $('#givenWord').val();
-      word = word.toLowerCase()
-      console.log(word)
+      word = word.toLowerCase();
+      word = $.trim(word);
+      console.log(word.split(" "));
+      word = word.split(" ")[0];
+      console.log(word);
+
       var ans = new Array;
       $.get('preceed_with_an.txt', function(data){
         var ans = data.split('\n');
