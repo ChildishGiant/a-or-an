cad382e3b683a808d7f8ae5406459f7d47ce9876
SolarPolarMan <solarpolarman@gmail.com>
2016-04-22 15:25:44 +0100

    now only downloads the file once.

--- jssource.js
+++ jssource.js
@@ -1,55 +1,37 @@
 var word = ""
-var arr = []
+var ans = new Array
+var counter = 0
+
 $(document).keypress(function(e) {
     if(e.which == 13) {
-
       word = $('#givenWord').val();
       word = word.toLowerCase();
       word = $.trim(word);
-      console.log(word.split(" "));
       word = word.split(" ")[0];
-      console.log(word);
-
-      var ans = new Array;
-      $.get('preceed_with_an.txt', function(data){
-        var ans = data.split('\n');
-        var answer = $("#answer");
-        for (var i = 0; i < ans.length; i++){
-          if (ans[i] === word && word != ""){
-            $(answer).text("An");
-            break;
-          }else{
-            $(answer).text("A");
-          }
-        }
-      });
+      if (counter == 0){
+        counter++
+        $.get('preceed_with_an.txt', function(data){
+          ans = data.split('\n')
+          console.log("loaded")
+          processResults();
+        });
+      }else{
+        processResults();
+      }
     }
 });
 
 function resizeInput() {
-
-    //Firstly take the content or placeholder if content is missing.
     var content =
         $(this).val().length > 0 ? $(this).val() : $(this).prop("placeholder");
-
-    //Create testing element with same content as input.
     var widthTester = $("<span>"+content+"</span>").hide();
-
-    //Place testing element into DOM after input (so it inherits same formatting as input does).
     widthTester.insertAfter($(this));
-
-    //Set inputs width; you may want to use outerWidth() or innerWidth()
-    //depending whether you want to count padding and border or not.
     $(this).css("width",widthTester.width()+"px");
-
-    //Remove the element from the DOM
     widthTester.remove();
  }
 
 $('input[type="text"]')
-    // event handler
     .keyup(resizeInput)
-    // resize on page load
     .each(resizeInput);
 
 $('#givenWord').focus(function() {
@@ -63,3 +45,15 @@ $('#givenWord').focus(function() {
             }
     }}(this)), 0);
 });
+
+function processResults() {
+  var answer = $("#answer");
+  for (var i = 0; i < ans.length; i++){
+    if (ans[i] === word && word != ""){
+      $(answer).text("An");
+      break;
+    }else{
+      $(answer).text("A");
+    }
+  }
+}
